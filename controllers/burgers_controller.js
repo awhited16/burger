4b1f3be8917d50935cecac9aef40ac1db7fe7c09
awhited16 <52683023+awhited16@users.noreply.github.com>
2019-11-25 21:25:44 -0500

    clean up code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var burgerFunctions = require("../models/burger");
 var router = express.Router();
-var connection = require("../config/connection");
+// var connection = require("../config/connection");
 
 router.get("/", function(req, res) {
   burgerFunctions.selectAll(function(data) {
@@ -13,6 +13,7 @@ router.get("/", function(req, res) {
 router.post("/api/burgers", function(req, res) {
   burgerFunctions.insertOne(["burger_name", "devoured"],[req.body.burger_name, false],
   function(result) {
+    res.json({ id: result.insertId });
     res.redirect("/");
   });
 });
@@ -20,17 +21,8 @@ router.post("/api/burgers", function(req, res) {
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
-  burgerFunctions.updateOne({ devoured: true }, condition, function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500);
-    }
-    else if (result.changedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
+  burgerFunctions.updateOne({ devoured: true }, condition, function(result) {
     res.redirect("/");
-    }
   });
 });
 
